dea78e3affec7e07a9aba35546fa98d3e9c6f557
kiboki1234 <andresboki2000@gmail.com>
2026-02-04 00:41:46 -0500

    update: base url frontend

--- backend/api.js
+++ backend/api.js
@@ -1,3 +1,3 @@
-//const API_BASE_URL = "https://awd-22962024-the-bug-slayer.vercel.app";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://sistema-de-recursos-humanos-b5oo.vercel.app";
+//const API_BASE_URL = "http://localhost:3000";
 module.exports = API_BASE_URL;
